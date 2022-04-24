d0c947bee6fa88ac4aa495a3a42faad35ac51ce2
milogreen <milogreenmilo@gmail.com>
2022-04-24 17:01:18 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 export default function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { 
+	HashRouter,
+	Routes,
+	Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 render(
 	<HashRouter>
-		<App />, rootElement
-	</HashRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+		</Routes>
+	</HashRouter>,
+	rootElement
 );
